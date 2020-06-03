7cb94a3c80262cab51a84b5049b6aeb02f4ef1e7
kosmos434 <kosmos434@protonmail.com>
2020-06-03 12:45:32 -0500

    add npm packages, make first tweet

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const config = require("./config.js");
+const Twitter = require("twitter");
 
 //animenewsnetwork id's go up to about 23333 as of 6/1/2020
 let randime = Math.floor(Math.random() * 23333);
 let url = "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=";
 let name;
 let type;
+let tweet;
 let sentenceStart = [
   "Dad!!!! I'm",
   "Dad get out!!! I'm",
@@ -14,6 +16,7 @@ let sentenceStart = [
   "Dad don't come in here!!! I'm",
   "UGH, I'm",
 ];
+let client = new Twitter(config);
 
 async function getStuff() {
   try {
@@ -35,16 +38,29 @@ function createString(n, t) {
     sentenceStart[Math.floor(Math.random() * sentenceStart.length)];
   let randExclamation = "!".repeat(Math.floor(Math.random() * 5));
   if (t === "manga") {
-    console.log(`${randStart} reading ${n}${randExclamation}`);
+    tweet = `${randStart} reading ${n}${randExclamation}`;
   } else {
-    console.log(`${randStart} watching ${n}${randExclamation}`);
+    tweet = `${randStart} watching ${n}${randExclamation}`;
   }
+}
+
+function tweetString() {
+  client.post("statuses/update", { status: tweet }, function (
+    error,
+    tweet,
+    response
+  ) {
+    if (error) throw error;
+    console.log(tweet); // Tweet body.
+    // console.log(response); // Raw response object.
+  });
 }
 
 async function main() {
   console.log(`the random number is...${randime}`);
   await getStuff();
   await createString(name, type);
+  await tweetString();
 }
 
 main();
